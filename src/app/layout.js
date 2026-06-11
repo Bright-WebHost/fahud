f962097bd81107fd98a8696c82c6de699a9f8ce0
Maithri <smaithri039@gmail.com>
2026-06-11 17:22:23 +0530

    Fix Vercel CSS asset paths by using GitHub Pages-specific prefix only

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import Script from "next/script";
 import "./globals.css";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const assetPrefix = isGitHubPages ? "/Fahud" : "";
+
 export const metadata = {
   title: "Fahud - Safety & Technocal trading",
   description: "A Life Full Of Design Secrets.",
@@ -11,15 +14,15 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* CSS Assets */}
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/bootstrap.min.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/font-awesome.min.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/owl.carousel.min.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/magnific-popup.min.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/swiper-bundle.min.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/style.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/bootstrap-icons.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/lc_lightbox.css`} />
-        <link rel="stylesheet" href={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/css/bootstrap-slider.min.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/bootstrap.min.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/font-awesome.min.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/owl.carousel.min.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/magnific-popup.min.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/swiper-bundle.min.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/style.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/bootstrap-icons.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/lc_lightbox.css`} />
+        <link rel="stylesheet" href={`${assetPrefix}/css/bootstrap-slider.min.css`} />
 
         {/* Fonts */}
         <link href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap" rel="stylesheet" />
@@ -31,24 +34,24 @@ export default function RootLayout({ children }) {
         </div>
 
         {/* JS Assets */}
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/jquery-3.7.1.min.js`} strategy="beforeInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/popper.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/bootstrap.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/magnific-popup.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/waypoints.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/counterup.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/isotope.pkgd.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/imagesloaded.pkgd.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/owl.carousel.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/theia-sticky-sidebar.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/jquery.bootstrap-touchspin.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/lc_lightbox.lite.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/bootstrap-slider.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/swiper-bundle.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/img-parallax.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/wow.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/gsap.min.js`} strategy="afterInteractive" />
-        <Script src={`${process.env.NODE_ENV === "production" ? "/Fahud" : ""}/js/custom.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/jquery-3.7.1.min.js`} strategy="beforeInteractive" />
+        <Script src={`${assetPrefix}/js/popper.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/bootstrap.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/magnific-popup.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/waypoints.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/counterup.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/isotope.pkgd.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/imagesloaded.pkgd.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/owl.carousel.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/theia-sticky-sidebar.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/jquery.bootstrap-touchspin.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/lc_lightbox.lite.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/bootstrap-slider.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/swiper-bundle.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/img-parallax.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/wow.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/gsap.min.js`} strategy="afterInteractive" />
+        <Script src={`${assetPrefix}/js/custom.js`} strategy="afterInteractive" />
 
         <div className="cursor"></div>
         <div className="cursor2"></div>
